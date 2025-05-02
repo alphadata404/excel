e40bb38443985eb638923635bb65d62b579f3611
alphadata404 <ajitkhanna408@gmail.com>
2025-05-02 17:53:26 +0530

    Delete src directory

--- src/components/ui/textarea.jsx
+++
@@ -1,3 +0,0 @@
-export function Textarea({ value, onChange, placeholder, className, rows }) {
-  return <textarea className={`border w-full p-2 ${className}`} rows={rows} placeholder={placeholder} value={value} onChange={onChange}></textarea>;
-}
